c44927614cc1f198b441d5af665e729df5261bdd
mselimdagdelen <95246917+mselimdagdelen@users.noreply.github.com>
2021-12-03 23:44:27 +0300

    new file

---
+++ suygulama_yeni/app/src/main/java/com/example/suygulama_yeni/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.suygulama_yeni;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_READ_CONTACTS = 1;
+    int resultValue;
+    ListView rehber_list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int a = 5;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rehber_list = findViewById(R.id.rehber_list);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)==PackageManager.PERMISSION_GRANTED) {
+            ArrayList<Kisiler> kisiler = new ArrayList<Kisiler>();
+
+            Cursor telefonun_rehberi = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while (telefonun_rehberi.moveToNext()) {
+                @SuppressLint("Range") String isim = telefonun_rehberi.getString(telefonun_rehberi.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+                @SuppressLint("Range") String numara = telefonun_rehberi.getString(telefonun_rehberi.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                @SuppressLint("Range") String contactID = telefonun_rehberi.getString(telefonun_rehberi.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+
+
+                Kisiler r_nesnesi = new Kisiler();
+
+                r_nesnesi.set_isim(isim);
+                r_nesnesi.set_numara(numara);
+                r_nesnesi.setResim(ContactPhoto(contactID));
+                kisiler.add(r_nesnesi);
+            }
+
+            telefonun_rehberi.close();
+            KisilerAdapter kisilerAdapter = new KisilerAdapter(this, kisiler);
+            if (rehber_list != null) {
+                rehber_list.setAdapter(kisilerAdapter);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},REQUEST_READ_CONTACTS);
+        }
+
+
+    }
+
+    public Bitmap ContactPhoto(String contactId) {
+        Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long.valueOf(contactId));
+        Uri photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        Cursor cursor = getContentResolver().query(photoUri,
+                new String[]{ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToNext();
+            byte[] data = cursor.getBlob(0);
+            if (data != null)
+                return BitmapFactory.decodeStream(new ByteArrayInputStream(data));
+            else
+                return null;
+        }
+        cursor.close();
+        return null;
+    }
+
+    public void git(View view) {
+        Intent intent = new Intent(this, son.class);
+        startActivity(intent);
+    }
+}
